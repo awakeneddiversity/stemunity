48d65f282ca84dfdd949107c96d961414f169096
awakeneddiversity <awakeneddiversityinitiative@gmail.com>
2021-01-15 23:43:45 -0800

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular.module('StemApp', [])
+
+    .controller('SearchController', SearchController);
+
+  SearchController.$inject = ['$scope'];
+
+  function SearchController($scope) {
+    // the input
+    $scope.searchItem = '';
+    // style object
+    $scope.styleProp = {};
+
+    // Check the meal intake input
+    $scope.searchDB = function() {
+
+    }
+  };
+
+})();
